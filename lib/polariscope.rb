f624da28b1db63f52ebb57b128d44cecc8b25e99
Lovro Bikic <lovro.bikic@infinum.hr>
2024-10-24 19:49:00 +0200

    Refactor polariscope module

--- lib/polariscope.rb
+++ lib/polariscope.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'polariscope/version'
-require_relative 'polariscope/scanner/codebase_health_score'
+require_relative 'polariscope/scanner/gemfile_health_score'
 require_relative 'polariscope/scanner/gem_versions'
 require_relative 'polariscope/file_content'
 
@@ -9,15 +9,15 @@ module Polariscope
   Error = Class.new(StandardError)
 
   class << self
-    def scan(gemfile_content: nil, gemfile_lock_content: nil, bundler_audit_config_content: nil)
-      Scanner::CodebaseHealthScore.new(
-        gemfile_content: gemfile_content || FileContent.for('Gemfile'),
-        gemfile_lock_content: gemfile_lock_content || FileContent.for('Gemfile.lock'),
-        bundler_audit_config_content: bundler_audit_config_content || FileContent.for('.bundler-audit.yml')
-      ).health_score
+    def scan(**opts)
+      Scanner::GemfileHealthScore.new(**opts.merge(
+        gemfile_content: opts.fetch(:gemfile_content, FileContent.for('Gemfile')),
+        gemfile_lock_content: opts.fetch(:gemfile_lock_content, FileContent.for('Gemfile.lock')),
+        bundler_audit_config_content: opts.fetch(:bundler_audit_config_content, FileContent.for('.bundler-audit.yml'))
+      )).health_score
     end
 
-    def gem_versions(dependency_names, spec_type: :released)
+    def gem_versions(dependency_names, spec_type: Scanner::DependencyContext::DEFAULT_SPEC_TYPE)
       Scanner::GemVersions.new(dependency_names, spec_type: spec_type)
     end
   end
